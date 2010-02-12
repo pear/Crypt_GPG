398fc1b8ed2e8768b0e82e7d58bf35ad58d7775c
Daniel O'Connor <clockwerx@php.net>
2010-02-12 00:14:05 +0000

    Fixed AllTests.php
    git-svn-id: https://svn.php.net/repository/pear/packages/Crypt_GPG/trunk@294941 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -42,7 +42,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Crypt_GPG_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'DecryptTestCase.php';
